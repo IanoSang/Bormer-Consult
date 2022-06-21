345bc086d9e896bca0495a27c552eb1cb50fb054
IanoSang <ianosang18@gmail.com>
2022-06-21 22:59:52 +0300

    feat: models- adds Inquiry, Message, CallRequest and Consultation class models

--- app/models.py
+++ app/models.py
@@ -29,10 +29,38 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
-    def save_profile(self):
-        self.user
-
     def delete_profile(self):
         self.delete()
 
 
+class Consultation(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='consultations')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.name} Consultation'
+
+    class Meta:
+        ordering = ["-pk"]
+
+
+class Inquiry(models.Model):
+    title = models.CharField(max_length=100)
+    consultation = models.ForeignKey(Consultation, on_delete=models.CASCADE, related_name='inquiries')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='inquiries')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=12, blank=True, null=True)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class CallRequest(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    phone = models.CharField(max_length=12, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
