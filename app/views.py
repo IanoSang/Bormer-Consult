b4707c1b5e66a2bed5072f3b546be90d19ca1bc5
IanoSang <ianosang18@gmail.com>
2022-06-21 09:53:12 +0300

    feat: views- adds a signup view funtion

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'base.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
