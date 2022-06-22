67de96e41c4f97e160bfda0fca99f526e4760620
IanoSang <ianosang18@gmail.com>
2022-06-22 22:19:41 +0300

    feat: views- adds a index view funtion to display the index page

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
 
 def signup(request):
