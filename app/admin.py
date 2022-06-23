856148a3ade97cb8ba0ebdf4739c42bbdfe43329
IanoSang <ianosang18@gmail.com>
2022-06-23 22:58:32 +0300

    feat: admin- registers all model classes to display in the admin site

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Consultation, Message, CallRequest, Inquiry
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Consultation)
+admin.site.register(Message)
+admin.site.register(CallRequest)
+admin.site.register(Inquiry)
