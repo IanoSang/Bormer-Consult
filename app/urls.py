11cfeb7fe9f7757afccd9c6e9e9ebd6dd7938092
IanoSang <ianosang18@gmail.com>
2022-06-23 13:54:17 +0300

    feat: urls- adds a url path to the profile page

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
+    path('profile/<username>/', views.profile, name='profile'),
 ]
