dd1dbb430c8cd82ff040006cba1c2e8b83c13a66
IanoSang <ianosang18@gmail.com>
2022-06-23 13:52:22 +0300

    feat: forms- adds a user update and profile update form

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,21 @@ class SignUpForm(UserCreationForm):
 
         for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'citizenship', 'profile_pic', 'bio']
+        help_texts = {
+            'name': None,
+            'email': None,
+        }
